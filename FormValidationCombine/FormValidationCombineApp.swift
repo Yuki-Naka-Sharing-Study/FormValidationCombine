751380ca46d25a0bd145e3aca7c645c35b6d4f22
仲優樹 <yu-ki216m@outlook.jp>
2025-06-25 19:15:20 +0900

    Initial Commit

---
+++ FormValidationCombine/FormValidationCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormValidationCombineApp.swift
+//  FormValidationCombine
+//
+//  Created by 仲優樹 on 2025/06/25.
+//
+
+import SwiftUI
+
+@main
+struct FormValidationCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
